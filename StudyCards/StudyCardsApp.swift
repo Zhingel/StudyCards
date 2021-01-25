f999680361fdf1be6af4afbb2a37dcefbfb1cdf7
Stas <staszhingel@icloud.com>
2021-01-25 13:40:56 +0300

    Initial Commit

---
+++ StudyCards/StudyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCardsApp.swift
+//  StudyCards
+//
+//  Created by Стас Жингель on 25.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct StudyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
